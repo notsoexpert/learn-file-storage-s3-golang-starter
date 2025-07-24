6368f849cd8b8f04974e3bdd4f40a3a3998e3fcd
notsoexpert <notsoexpert@gmail.com>
2025-07-24 15:50:20 -0500

    Completed CH1/2 and CH3L1-2

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/base64"
 	"fmt"
+	"io"
+	"mime"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
@@ -28,10 +35,73 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
-	// TODO: implement the upload here
+	const maxMemory = 10 << 20
+	r.ParseMultipartForm(maxMemory)
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+		return
+	}
+	defer file.Close()
+
+	mediaType, _, err := mime.ParseMediaType(header.Header.Get("Content-Type"))
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Bad content type", err)
+		return
+	}
+
+	var extension string
+	if strings.Contains(mediaType, "image/png") {
+		extension = "png"
+	} else if strings.Contains(mediaType, "image/jpeg") {
+		extension = "jpeg"
+	} else {
+		respondWithError(w, http.StatusBadRequest, "Invalid thumbnail type", err)
+		return
+	}
+
+	video, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to find matching data", err)
+		return
+	}
+
+	if video.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "Unauthorized request", err)
+		return
+	}
+
+	var fnBytes []byte = make([]byte, 32)
+	_, _ = rand.Read(fnBytes)
+	fileName := base64.RawURLEncoding.EncodeToString(fnBytes)
+
+	filePath := filepath.Join(cfg.assetsRoot, fileName+"."+extension)
+	osFile, err := os.Create(filePath)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Unable to store content", err)
+		return
+	}
+	defer osFile.Close()
+
+	_, err = io.Copy(osFile, file)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Write failure", err)
+		return
+	}
+
+	if video.ThumbnailURL == nil {
+		video.ThumbnailURL = new(string)
+	}
+	*video.ThumbnailURL = fmt.Sprintf(`http://localhost:%s/assets/%s.%s`, cfg.port, fileName, extension)
+
+	err = cfg.db.UpdateVideo(video)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Failed to update database", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, video)
 }
